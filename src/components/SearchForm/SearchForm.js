1523d9b0c52c085858cc70e29ff042f9d80e8e16
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-01 00:24:33 -0600

    Add initial function for searching news

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './SearchForm.css';
 
-const SearchForm = () => {
-  return <header className="header">
-    <h1 className="app-name">What's <span className="yellow">News?</span></h1>
-    <input className="search__input" placeholder="Search for news articles here..."></input>
-    <div className="search__btn">Search Now</div>
-  </header>
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search:''
+    }
+  }
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({[e.target.name]:e.target.value});
+    console.log(this.state.search)
+  }
+
+  handleSubmit = (e) => {
+    // const searchText = this.state.search;
+    // this.props.searchNews(searchText, e);
+    this.setState({ search:'' });
+    console.log(this.state.search)
+  }
+
+  render() {
+    return <header className='header'>
+      <h1 className='app-name'>What's <span className='yellow'>News?</span></h1>
+      <input 
+        type= 'text'
+        name= 'search'
+        value= {this.state.search}
+        onChange= {this.handleChange}
+        className='search__input' 
+        placeholder='Search for news articles here...'
+      />
+      <div 
+      className='search__btn'
+      onClick= {this.handleSubmit}
+      >
+        Search Now</div>
+    </header>
+  }
  }
 
 export default SearchForm;
