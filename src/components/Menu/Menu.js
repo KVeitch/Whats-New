28cd3030abe1233114f59c71671128e1b5288d2c
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-30 18:33:27 -0600

    Add initial Menu styling

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './Menu.css'
 
+const Menu = () => {
+  return <div className="navbar">
+    <ul className="btns-list">
+      <li>Local News</li>
+      <li>Technology</li>
+      <li>Entertainment</li>
+      <li>Science</li>
+      <li className="active-menu">Health</li>
+    </ul>
+  </div>
+}
 // MENU COMPONENT CODE GOES HERE
 
 export default Menu;
