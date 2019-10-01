ef6c35a9e6f11946975edd205a15b7af742fab3f
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-30 18:33:00 -0600

    Add initial styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import Menu from '../Menu/Menu';
+import SearchForm from '../SearchForm/SearchForm';
+import NewsContainer from '../NewsContainer/NewsContainer';
+
 
 class App extends Component {
   constructor() {
@@ -13,7 +17,9 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <SearchForm />
+        <Menu />
+        <NewsContainer />
       </div>
     );
   }
