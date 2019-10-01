338585ae213b8354f21fe952685a1eed18ad2f68
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-30 23:25:37 -0600

    Add icons for the menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './Menu.css'
+import location from '../../images/location.svg'
+import entertainment from '../../images/entertainment.svg'
+import science from '../../images/science.svg'
+import health from '../../images/health.svg'
+import technology from '../../images/technology.svg'
 
 const Menu = (data) => {
   return <div className='navbar'>
     <ul className='btns-list'>
-      <li className='active-menu' id='local' onClick={e => data.changeNewsFeed(e)}>Local News</li>
-      <li id='technology' onClick={e => data.changeNewsFeed(e)}>Technology</li>
-      <li id='entertainment' onClick={e => data.changeNewsFeed(e)}>Entertainment</li>
-      <li id='science' onClick={e => data.changeNewsFeed(e)}>Science</li>
-      <li id='health' onClick={e => data.changeNewsFeed(e)}>Health</li>
+      <li className='active-menu' id='local' onClick={e => data.changeNewsFeed(e)}>
+        <img src={location} className="icon" alt='location marker'/>
+        Local News
+      </li>
+      <li id='technology' onClick={e => data.changeNewsFeed(e)}>
+        <img src={technology} className="icon" alt='location marker'/>
+        Technology
+      </li>
+      <li id='entertainment' onClick={e => data.changeNewsFeed(e)}>
+        <img src={entertainment} className="icon" alt='location marker'/>
+        Entertainment
+      </li>
+      <li id='science' onClick={e => data.changeNewsFeed(e)}>
+        <img src={science} className="icon" alt='location marker'/>
+        Science
+      </li>
+      <li id='health' onClick={e => data.changeNewsFeed(e)}>
+        <img src={health} className="icon" alt='location marker'/>
+        Health
+      </li>
     </ul>
   </div>
 }
