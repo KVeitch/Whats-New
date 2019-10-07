5f76532306811d8634f7515c5ad97120658c0fd3
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-07 00:26:18 -0600

    Updates

---
+++ precache-manifest.de119c754ab0b9fe1e9ba22a82dee39e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821b967c25d2b6a0272cc4833f6b895b",
+    "url": "/Whats-New/index.html"
+  },
+  {
+    "revision": "3a6b4832e0169ae2fc13",
+    "url": "/Whats-New/static/css/main.28543bf9.chunk.css"
+  },
+  {
+    "revision": "c9a977a033552ccc20cc",
+    "url": "/Whats-New/static/js/2.563b28a4.chunk.js"
+  },
+  {
+    "revision": "3a6b4832e0169ae2fc13",
+    "url": "/Whats-New/static/js/main.baac2270.chunk.js"
+  },
+  {
+    "revision": "1de73eab390750baa94b",
+    "url": "/Whats-New/static/js/runtime-main.37d1c27e.js"
+  },
+  {
+    "revision": "269bffbdfcfb823e0316b6d3b74cecde",
+    "url": "/Whats-New/static/media/arrow-to-right.269bffbd.svg"
+  },
+  {
+    "revision": "e8f1810331c366c2422e30ff002377ec",
+    "url": "/Whats-New/static/media/entertainment.e8f18103.svg"
+  },
+  {
+    "revision": "537673a8056783e32c267afe637bb3a2",
+    "url": "/Whats-New/static/media/health.537673a8.svg"
+  },
+  {
+    "revision": "e0bd0282cc73699f4ec5817f4f0e6fb8",
+    "url": "/Whats-New/static/media/location.e0bd0282.svg"
+  },
+  {
+    "revision": "d4891c9b2c7b4ff24f596763b9155e35",
+    "url": "/Whats-New/static/media/science.d4891c9b.svg"
+  },
+  {
+    "revision": "b94f4bc81a1bff60890dd05218006cd5",
+    "url": "/Whats-New/static/media/technology.b94f4bc8.svg"
+  }
+]);
